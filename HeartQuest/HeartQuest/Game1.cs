9dfaabfba66842d880540a6db5f64a3512ca4af1
Justin Furtado <justin.l.furtado@gmail.com>
2016-04-30 17:23:17 -0600

    V 0.0.2

--- HeartQuest/HeartQuest/Game1.cs
+++ HeartQuest/HeartQuest/Game1.cs
@@ -49,7 +49,7 @@ namespace HeartQuest
             menuCorner = Content.Load<Texture2D>("MenuCorner");
             menuBar = Content.Load<Texture2D>("MenuSide");
             menuCenter = Content.Load<Texture2D>("Center");
-            menu = new Menu(test, Vector2.Zero, menuCorner, menuBar, menuCenter, 50, 30, font72);
+            menu = new Menu(test, new Vector2(240, 240), menuCorner, menuBar, menuCenter, 20, 10, font16);
         }
         
         protected override void UnloadContent()
@@ -74,13 +74,13 @@ namespace HeartQuest
                         {
                             gameState = GameState.PLAYING;
                             string[] options = { "Win", "Lose" };
-                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font72);
+                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font16);
                         }
                         else if (menu.Selection == 1)
                         {
                             gameState = GameState.INFO_SCREEN;
                             string[] options = { "Return to title", "Break game" };
-                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font72);
+                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font16);
                         }
                         // TODO handle other options
                     }
@@ -96,14 +96,14 @@ namespace HeartQuest
                         {
                             gameState = GameState.VICTORY_SCREEN;
                             string[] options = { "Return to Title", "Break game" };
-                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font72);
+                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font16);
                         }
 
                         if (menu.Selection == 1)
                         {
                             gameState = GameState.GAMEOVER_SCREEN;
                             string[] options = { "Return to title", "Break game" };
-                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font72);
+                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font16);
                         }
                     }
                     break;
@@ -118,7 +118,7 @@ namespace HeartQuest
                         {
                             gameState = GameState.TITLE_SCREEN;
                             string[] options = { "Start game", "Info Screen" };
-                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font72);
+                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font16);
                         }
                         
                     }
@@ -134,7 +134,7 @@ namespace HeartQuest
                         {
                             gameState = GameState.TITLE_SCREEN;
                             string[] options = { "Start game", "Info Screen" };
-                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font72);
+                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font16);
                         }
                         
                     }
@@ -150,7 +150,7 @@ namespace HeartQuest
                         {
                             gameState = GameState.TITLE_SCREEN;
                             string[] options = { "Start game", "Info Screen" };
-                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font72);
+                            menu = new Menu(options, menu.Position, menuCorner, menuBar, menuCenter, menu.Width, menu.Height, font16);
                         }
                         
                     }
@@ -170,25 +170,62 @@ namespace HeartQuest
             switch (gameState)
             {
                 case GameState.TITLE_SCREEN:
+                    string titleScreenText = "Heart Quest";
+                    Vector2 textDimmensions = font72.MeasureString(titleScreenText);
+                    spriteBatch.DrawString(font72, titleScreenText, new Vector2((GraphicsDevice.Viewport.Width - textDimmensions.X)/2.0f, 50), Color.Black);
+                    string creditsText = "Made by Devin Duren and Justin Furtado";
+                    Vector2 otherText = font16.MeasureString(creditsText);
+                    spriteBatch.DrawString(font16, creditsText, new Vector2((GraphicsDevice.Viewport.Width - otherText.X) / 2.0f, GraphicsDevice.Viewport.Height - 25), Color.Black);
                     menu.Draw(spriteBatch);
                     break;
 
                 case GameState.PLAYING:
+                    string todo = "INSERT GAME HERE";
+                    Vector2 temp = font72.MeasureString(todo);
+                    spriteBatch.DrawString(font72, todo, new Vector2((GraphicsDevice.Viewport.Width - temp.X) / 2.0f, 50), Color.Black);
                     menu.Draw(spriteBatch);
 
                     break;
 
                 case GameState.INFO_SCREEN:
+                    string infoText = "Made the weekened of April 29 2016 at Neumont University for a Game Jam";
+                    string infoText2 = "Created by Justin Furtado and Devin Duren";
+                    string infoText3 = "Special thanks to Blake Dennis (ideas) and Doug Gatto (motivation/rectangles)";
+                    Vector2 d1 = font16.MeasureString(infoText);
+                    Vector2 d2 = font16.MeasureString(infoText2);
+                    Vector2 d3 = font16.MeasureString(infoText3);
+                    spriteBatch.DrawString(font16, infoText, new Vector2(50, 50), Color.Black);
+                    spriteBatch.DrawString(font16, infoText2, new Vector2(50, 75), Color.Black);
+                    spriteBatch.DrawString(font16, infoText3, new Vector2(50, 100), Color.Black);
+
                     menu.Draw(spriteBatch);
 
                     break;
 
                 case GameState.VICTORY_SCREEN:
+                    string victoryText = "Conratulations!";
+                    string thanks = "Hope you enjoyed playing HeartQuest.";
+                    Vector2 t1 = font72.MeasureString(victoryText);
+                    Vector2 t2 = font16.MeasureString(thanks);
+                    spriteBatch.DrawString(font72, victoryText, new Vector2((GraphicsDevice.Viewport.Width - t1.X) / 2.0f, 50), Color.Black);
+                    spriteBatch.DrawString(font16, thanks, new Vector2((GraphicsDevice.Viewport.Width - t2.X) / 2.0f, 150), Color.Black);
+                    creditsText = "Made by Devin Duren and Justin Furtado";
+                    otherText = font16.MeasureString(creditsText);
+                    spriteBatch.DrawString(font16, creditsText, new Vector2((GraphicsDevice.Viewport.Width - otherText.X) / 2.0f, GraphicsDevice.Viewport.Height - 25), Color.Black);
                     menu.Draw(spriteBatch);
 
                     break;
 
                 case GameState.GAMEOVER_SCREEN:
+                    string gameOverText = "Game Over!";
+                    thanks = "Hope you enjoyed playing HeartQuest.";
+                    t1 = font72.MeasureString(gameOverText);
+                    t2 = font16.MeasureString(thanks);
+                    spriteBatch.DrawString(font72, gameOverText, new Vector2((GraphicsDevice.Viewport.Width - t1.X) / 2.0f, 50), Color.Black);
+                    spriteBatch.DrawString(font16, thanks, new Vector2((GraphicsDevice.Viewport.Width - t2.X) / 2.0f, 150), Color.Black);
+                    creditsText = "Made by Devin Duren and Justin Furtado";
+                    otherText = font16.MeasureString(creditsText);
+                    spriteBatch.DrawString(font16, creditsText, new Vector2((GraphicsDevice.Viewport.Width - otherText.X) / 2.0f, GraphicsDevice.Viewport.Height - 25), Color.Black);
                     menu.Draw(spriteBatch);
 
                     break;
